1c5271c169dc4760caf9f72fb60ad5702e794a52
playajames <kennedy.james94@gmail.com>
2022-01-17 12:59:48 -0800

    Removed systems test

--- src/main/java/me/playajames/flagslib/flagslib/FlagsLib.java
+++ src/main/java/me/playajames/flagslib/flagslib/FlagsLib.java
@@ -7,7 +7,9 @@ import java.util.Objects;
 
 public final class FlagsLib extends JavaPlugin {
 
+
     public static StorageType STORAGETYPE = StorageType.File;
+
 
     @Override
     public void onEnable() {
@@ -17,10 +19,12 @@ public final class FlagsLib extends JavaPlugin {
         this.getLogger().info("FlagsLib has been enabled.");
     }
 
+
     @Override
     public void onDisable() {
         this.getLogger().info("FlagsLib has been disabled.");
     }
+
 
     private void setConfigSettings() {
         FileConfiguration config = getConfig();
@@ -28,14 +32,5 @@ public final class FlagsLib extends JavaPlugin {
             STORAGETYPE = StorageType.MySQL;
     }
 
-    // TODO Not a very thorough test, should be fixed before implemented
-    private void testSystems() {
-        FlagManager.setGlobalFlag("q9c8w74yrna987nyhm", null);
-        FlagManager.getGlobalFlag("q9c8w74yrna987nyhm").delete();
-        if (!FlagManager.hasGlobalFlag("q9c8w74yrna987nyhm"))
-            FlagManager.setGlobalFlag("flags-lib-test-passed", "true");
-        else
-            FlagManager.setGlobalFlag("flags-lib-test-passed", "false");
-    }
 
 }
